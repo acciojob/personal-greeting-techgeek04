e496e5df1e468c5e5a69b98962ab1a5b35ad8792
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-01-27 21:27:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
+import {usestate} from "react";
 
+const [name, setName] = useState('');
+const [greeting, setGreeting] = useState('');
+const submitButton = (e) => {
+  e.preventDefault();
+    if (name.trim()) {
+      setGreeting(`Hello, ${name}!`);
+    } else {
+      setGreeting('');
+    }
+}
 const App = () => {
   return (
-    <div>
+    <div onSubmit = {submitButton}>
         {/* Do not remove the main div */}
+    <label>Enter your name:</label>
+    <input type = "text" value = {name} onChange = {(e) => setName(e.target.value)} />
+    <button type = "submit">Submit</button>
     </div>
   )
 }
